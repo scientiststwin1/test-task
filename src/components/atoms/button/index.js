8e40daf55ad3b677d99b84257ffcf7fba748c6c5
Twin Ali <scientists.twin@gmail.com>
2022-03-17 16:29:51 +0400

    feature: design button handlled successfully

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -1,22 +1,41 @@
 import React, { Children } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const Button = (props) => {
 
-  const { onClick, type, Children } = props
+  const { onClick, type, size, children, className, ...prop } = props;
+
+  const styleSM = 'px-2.5 py-1.5 text-xs'
+  const styleMD = 'px-4 py-2 text-sm'
+  const styleLG = 'px-6 py-3 text-lg'
+
+  const combineClassNames = classnames(
+    'text-white font-bold border-none outline-none text-sm',
+    {
+      'bg-emerald-700 hover:bg-emerald-800': type === 'submit',
+      'bg-red-600 hover:bg-red-700': type === 'delete',
+    },
+    {
+      [styleSM]: size === 'sm',
+      [styleMD]: size === 'md',
+      [styleLG]: size === 'lg',
+    },
+    className)
 
   return (
-    <button type={type} onClick={onClick} >
-        {Children}
+    <button onClick={onClick} className={combineClassNames} >
+      {children}
     </button>
   )
 }
 
 Button.prototype = {
-    onClick: PropTypes.func,
-    type: PropTypes.oneOf(['submit', 'reset', 'button']),
-    Children: PropTypes.element,
-
+  onClick: PropTypes.func,
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+  type: PropTypes.oneOf(['submit', 'delete']),
+  Children: PropTypes.element,
+  className: PropTypes.string,
 }
 
 export default Button;
