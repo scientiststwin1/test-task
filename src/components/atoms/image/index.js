22a36651a5f9649ad6c441280df05963d752e47d
Twin Ali <scientists.twin@gmail.com>
2022-03-18 12:06:40 +0400

    feature: add className prop to image atom

--- src/components/atoms/image/index.js
+++ src/components/atoms/image/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Image = (props) => {
 
-    const { src, alt } = props
+    const { src, alt, className } = props
 
     return (
-        <img src={src} alt={alt} />
+        <img src={src} alt={alt} className={className} />
     )
+}
+
+Image.prototype = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
 }
 
 export default Image;
