b26d570b1288426b20fcec5cc9ac26d6e65f30b2
Twin Ali <scientists.twin@gmail.com>
2022-03-18 13:17:08 +0400

    feature: initialize workSamples organism

---
+++ src/components/organisms/workSamples/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ImageBox from '../../molecules/imageBox';
+
+const WorkSamples = () => {
+    return (
+        <div className=' grid grid-cols-3 gap-x-3 gap-y-3'>
+            <ImageBox src={require('../../../assest/images/container_4.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_5.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_6.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_8.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_9.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_4.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_5.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_6.jpg')} />
+            <ImageBox src={require('../../../assest/images/container_8.jpg')} />
+        </div>
+    )
+}
+
+export default WorkSamples;
