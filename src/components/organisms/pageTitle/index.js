6ad61667916916492c60f02e8cf315e732f291f1
Twin Ali <scientists.twin@gmail.com>
2022-03-18 15:01:38 +0400

    feature: initialize pageTitle Organism

---
+++ src/components/organisms/pageTitle/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Subtitle from '../../atoms/subtitle';
+
+const PageTitle = props => {
+
+    const { title } = props;
+    
+    return (
+        <div className="w-full bg-emerald-600 py-6">
+            <Subtitle className="w-[75vw] m-auto text-white font-bold text-xl">
+                {title}
+            </Subtitle>
+        </div>
+    )
+}
+
+export default PageTitle;
