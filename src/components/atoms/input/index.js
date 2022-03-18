45aef854951530ec073bed8277f78b84b20423d0
Twin Ali <scientists.twin@gmail.com>
2022-03-18 13:56:25 +0400

    feature: input atom updated successfully

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Classnames from 'classnames';
 
 const Input = (props) => {
 
-  const { type } = props;
+  const { className, placeholder, type, ...prop } = props
+
+  const combineClasses = Classnames(
+    'outline-none border border-[2px] ',
+    'border-gray-500 px-2 py-5 h-8',
+    className,
+  )
 
   return (
-    <input type={type} />
+    <input type={type} className={combineClasses} placeholder={placeholder} {...prop} />
   )
 }
 
 Input.prototype = {
-    type: PropTypes.string,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
 }
 
 export default Input;
