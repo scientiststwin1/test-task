54ec0976845e67bc5ff300270d9ba70b54c187a8
Twin Ali <scientists.twin@gmail.com>
2022-03-19 10:29:43 +0400

    feature: add basic element to about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,77 @@
-import React from 'react'
+import React from 'react';
+import Subtitle from '../../components/atoms/subtitle'
+import Text from '../../components/atoms/text';
+import ServiceBox from '../../components/molecules/serviceBox';
+import PageTitle from '../../components/organisms/pageTitle';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+      <PageTitle title="ABOUT MY BUSINESS" />
+
+      <div className="w-full">
+        <div className="w-[75vw] text-gray-500 m-auto py-3 divide-y-[2rem] divide-transparent ">
+          <div className="w-full grid grid-cols-[45%_53%] justify-between  ">
+            <div className="bg-slate-300"> ok </div>
+            <div className="divide-y-8 divide-transparent">
+              <Text>
+                Lorem nn Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type
+                specimen book. It has survived not only five centuries, but
+                also the leap into electronic typesetting, remaining
+                essentially unchanged.
+              </Text>
+            </div>
+          </div>
+
+          <div className="w-full grid grid-cols-[53%_45%] justify-between">
+            <div className="space-y-4">
+              <Subtitle className="font-bold">MISSION STATEMENT </Subtitle>
+              <Text>
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type
+                specimen book. It has survived not only five centuries, but
+                also the leap into electronic typesetting, remaining
+                essentially unchanged.
+              </Text>
+            </div>
+            <div className="space-y-4">
+              <Subtitle className="font-bold"> FUN FACTS</Subtitle>
+              <Text>
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard dummy text ever since the 1500s, when an unknown
+                printer took a galley of type and scrambled it to make a type
+                specimen book. It has survived not only five centuries, but
+                also the leap into electronic typesetting, remaining
+                essentially unchanged.
+              </Text>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <div className="w-[75vw] m-auto">
+          <Subtitle>SERVICES</Subtitle>
+        </div>
+
+        <div className="bg-gray-200">
+          <div className="w-[75vw] m-auto grid grid-cols-4 px-12 ">
+            <ServiceBox text="WEBSITES" />
+            <ServiceBox text="PHOTOGRAPHY" />
+            <ServiceBox text="SEO" />
+            <ServiceBox text="APPLICATIONS" />
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
