a1dd3fdabbf0bc2b5de11a059992eb42139be128
Twin Ali <scientists.twin@gmail.com>
2022-03-20 12:06:09 +0400

    feature: initialize tablet molecule

---
+++ src/components/molecules/tablet/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from '../../atoms/image';
+
+const Tablet = () => {
+  return (
+    <div className='relative' >
+        <Image className="absolute top-4 left-36 z-10" src={require('../../../assest/images/tablet/camera_0.png')} />
+        <Image className="absolute inset-0" src={require('../../../assest/images/tablet/frame.png')} />
+        <Image className="absolute top-10 left-8" src={require('../../../assest/images/tablet/screen_0.png')} />
+        <Image className="absolute top-[27rem] left-36 z-10" src={require('../../../assest/images/tablet/button.png')} />
+    </div>
+  )
+}
+
+export default Tablet; 
