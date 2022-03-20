3c5b4915791febb2a059b870311b82465744273c
Twin Ali <scientists.twin@gmail.com>
2022-03-20 12:18:59 +0400

    feature: initialize pc molecule

---
+++ src/components/molecules/pc/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from '../../atoms/image'
+
+const Pc = () => {
+  return (
+    <div className='relative' >
+        <Image className='absolute top-3 left-[15rem] z-10' src={require('../../../assest/images/pc/camera.png')} alt='pc_camera' />
+        <Image className='absolute top-0 left-0' src={require('../../../assest/images/pc/case.png')} alt='pc_case' />
+        <Image className='absolute top-7 left-6' src={require('../../../assest/images/pc/screen.png')} alt='pc_screen' />
+        <Image className='absolute top-[20.7rem] left-48' src={require('../../../assest/images/pc/indent.png')} alt='pc_indent' />
+
+    </div>
+  )
+}
+
+export default Pc
