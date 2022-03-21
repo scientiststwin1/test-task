c60950a603989a0561294f07159c9fd5b21f8388
Twin Ali <scientists.twin@gmail.com>
2022-03-21 14:45:36 +0400

    fix: service box style fixed

--- src/components/molecules/serviceBox/index.js
+++ src/components/molecules/serviceBox/index.js
@@ -3,15 +3,15 @@ import React from 'react'
 
 const ServiceBox = (props) => {
 
-  const { icon, text } = props;
+  const { icon, text, onClick } = props;
 
   const combineClassNames = classnames(
-    'flex flex-col items-center justify-center py-20 bg-gray-200 hover:bg-gray-300 hover:cursor-pointer',
-    'space-y-4'
+    'flex flex-col items-center justify-center py-14 bg-gray-200 hover:bg-gray-300 hover:cursor-pointer',
+    'space-y-4 font-bold text-main-gray hover:text-black fill-main-gray hover:fill-black'
   )
 
   return (
-    <div className={combineClassNames}>
+    <div className={combineClassNames} onClick={onClick} >
       { icon }
       <h3>{text}</h3>
     </div>
