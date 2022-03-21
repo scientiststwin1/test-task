1cdeadd8826ab7b6946c37732b56e87acaf2f3c7
Twin Ali <scientists.twin@gmail.com>
2022-03-21 14:50:25 +0400

    feature: add onClick to arrowLeftIcon and arrowRightIcon

--- src/components/atoms/icons/arrowLeftIcon.js
+++ src/components/atoms/icons/arrowLeftIcon.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ArrowLeftIcon = props => {
 
-    const { className } = props;
+    const { className, onClick } = props;
 
     return (
-        <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <svg className={className} onClick={onClick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path fill="none" d="M0 0h24v24H0V0z" /><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z" />
         </svg>
     )
--- src/components/atoms/icons/arrowRightIcon.js
+++ src/components/atoms/icons/arrowRightIcon.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ArrowRightIcon = props => {
 
-    const { className } = props;
+    const { className, onClick } = props;
 
     return (
-        <svg className={className}  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" />
+        <svg className={className} onClick={onClick} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" />
         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z" />
         </svg>
     )
