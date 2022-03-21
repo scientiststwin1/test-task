b13192e3bc68f389028aefc26a12c8607661402f
Twin Ali <scientists.twin@gmail.com>
2022-03-21 17:18:29 +0400

    feature: add header and footer

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import Home from './pages/home';
 import Contact from './pages/contact';
 import Work from './pages/work';
 
+import Footer from './components/organisms/footer';
+import Header from './components/organisms/header';
+
 function App() {
   return (
+    <>
+      <Header />
       <Routes>
 
-        <Route path='/' element={<Home />}  />
+        <Route path='/' element={<Home />} />
 
         <Route path='/about' element={<About />} />
 
         <Route path='/contact' element={<Contact />} />
-          
+
         <Route path='/work' element={<Work />} />
-      
-      </Routes>      
+
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
