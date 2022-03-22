5cae80bf97f78e996bd409ca58f4de527e4a0f62
Twin Ali <scientists.twin@gmail.com>
2022-03-22 11:53:55 +0400

    fix: end-point added to form

--- src/components/organisms/contact/contactForm.js
+++ src/components/organisms/contact/contactForm.js
@@ -1,17 +1,59 @@
+import { useState } from 'react';
 import TextArea from '../../atoms/textArea';
 import Input from '../../atoms/input';
 import Button from '../../atoms/button';
+import Text from '../../atoms/text';
+import ContactService from '../../../service/contact.service';
+
+const contactService = new ContactService();
 
 const ContactForm = () => {
-    return (
-        <form className='space-y-3'>
-            <Input required className="w-3/4" type="text" placeholder="Name" />
-            <Input required className="w-3/4" type="email" placeholder="Email Address" />
-            <Input className="w-3/4" type="text" placeholder="Subject" />
-            <TextArea required maxlength="1000" className="w-full h-48 " ></TextArea>
 
-            <Button type="submit" size="sm">SEND MESSAGE</Button>
-        </form>
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [subject, setSubject] = useState("")
+    const [description, setdescription] = useState("")
+
+    const [information, setInformation] = useState(null)
+
+    const updateState = setState => event => {
+        event.preventDefault();
+        setState(event.target.value)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        contactService.create({
+            name: name,
+            email: email,
+            subject: subject,
+            description: description,
+        }).then(response => {
+            setInformation(description)
+        }).catch(error => {
+            alert("Failed submit form")
+        })
+    }
+
+
+    return (
+        <>
+            {information ?
+                <Text>
+                    {information}
+                </Text>
+                :
+                <form className='space-y-3' onSubmit={handleSubmit} >
+                    <Input onChange={updateState(setName)} required className="w-3/4" type="text" placeholder="Name" />
+                    <Input onChange={updateState(setEmail)} required className="w-3/4" type="email" placeholder="Email Address" />
+                    <Input onChange={updateState(setSubject)} className="w-3/4" type="text" placeholder="Subject" />
+                    <TextArea onChange={updateState(setdescription)} required maxlength="1000" className="w-full h-48 " ></TextArea>
+
+                    <Button type="submit" size="sm">SEND MESSAGE</Button>
+                </form>
+            }
+        </>
     )
 }
 
