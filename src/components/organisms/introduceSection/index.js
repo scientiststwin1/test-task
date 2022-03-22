ad3060a32940dd921100d8a250b42fe2fc44f5e0
Twin Ali <scientists.twin@gmail.com>
2022-03-22 12:08:10 +0400

    feature: add youtube modal to introducesection

--- src/components/organisms/introduceSection/index.js
+++ src/components/organisms/introduceSection/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlayIcon from '../../atoms/icons/playIcon';
 import Subtitle from '../../atoms/subtitle';
 import Text from '../../atoms/text';
+import YoutubeModal from '../../molecules/youtubeModal';
 
 const IntroduceSection = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <section className=' bg-gray-200 py-10 box-content w-full h-52'>
 
             <div className='w-[75%] m-auto h-full grid grid-cols-[1fr_1.5fr] gap-x-5' >
 
-                <div className='bg-orange-400 flex justify-center items-center' >
+                <div onClick={setIsOpen.bind(this, true)} className='bg-orange-400 flex justify-center items-center hover:cursor-pointer' >
                     <PlayIcon className="w-20 fill-stone-200" />
                 </div>
                 <div className='w-full' >
@@ -22,9 +26,10 @@ const IntroduceSection = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </Text>
                 </div>
-
             </div>
+            {isOpen && <YoutubeModal videoId='4UZrsTqkcW4' onClose={setIsOpen.bind(this, false)} />  }
         </section>
+
     )
 }
 
