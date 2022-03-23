087b91ae08d7fba0fe2c10ead647f344b775c60f
Twin Ali <scientists.twin@gmail.com>
2022-03-23 12:14:12 +0400

    feature: add responsive design to workSample

--- src/components/organisms/workSamples/index.js
+++ src/components/organisms/workSamples/index.js
@@ -3,7 +3,7 @@ import ImageBox from '../../molecules/imageBox';
 
 const WorkSamples = () => {
     return (
-        <div className=' grid grid-cols-3 gap-x-3 gap-y-3'>
+        <div className=' grid grid-cols-1 md:grid-cols-3 gap-x-3 gap-y-3'>
             <ImageBox src={require('../../../assest/images/container_4.jpg')} />
             <ImageBox src={require('../../../assest/images/container_5.jpg')} />
             <ImageBox src={require('../../../assest/images/container_6.jpg')} />
