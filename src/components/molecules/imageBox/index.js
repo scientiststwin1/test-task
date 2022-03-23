16d5a1d6ddcba3ddcb4df307c3dc72cd8bbe8d17
Twin Ali <scientists.twin@gmail.com>
2022-03-23 15:33:24 +0400

    feature: add style for hover

--- src/components/molecules/imageBox/index.js
+++ src/components/molecules/imageBox/index.js
@@ -9,14 +9,14 @@ const ImageBox = props => {
   const { src, alt, className } = props;
 
   const combineClasses = classnames(
-    'relative group w-full h-52 2xl:h-80 hover:cursor-pointer',
+    'relative group w-full h-52 2xl:h-80 hover:cursor-pointer hover:opacity-80',
     className
   )
 
   return (
     <div className={combineClasses} >
 
-        <Image src={src} alt={alt} className="w-full h-full block object-conver " />
+        <Image src={src} alt={alt} className="w-full h-full block object-conver" />
 
         <div className='w-20 h-20 absolute hidden top-1/2 left-1/2 group-hover:flex justify-center items-center transform -translate-x-1/2 -translate-y-1/2 bg-green-500 rounded-full '>
           <ChainIcon className="fill-white w-10" />
