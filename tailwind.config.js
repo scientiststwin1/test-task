c1275f33c19425917c8101637de2b59a8a7ae67a
Twin Ali <scientists.twin@gmail.com>
2022-03-23 13:42:21 +0400

    feature: add animation showElement

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
         'main-green': '#009000',
         'main-gray': '#6b7280',
       },
+      keyframes: {
+        showElement: {
+          '0%': {
+            opacity: '0'
+          },
+          '50%': {
+            opacity: '0'
+          },
+          '100': {
+            opacity: '10'
+          }
+        }
+      },
     },
   },
   plugins: [],
