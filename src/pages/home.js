e9b29876a3243d80eea5b05ab6c50e0a176b20a4
Twin Ali <scientists.twin@gmail.com>
2022-03-23 16:02:59 +0400

    feature: add helemet to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,17 @@ import React from 'react';
 import MainHomeSection from '../components/organisms/mainHomeSection';
 import IntroduceSection from '../components/organisms/introduceSection';
 import FeaturedSection from '../components/organisms/featuredSection';
+import { Helmet } from 'react-helmet';
 
 
 const Home = () => {
 
   return (
     <div>
+      <Helmet>
+        <title>Home</title>
+        <meta name="description" content="It is Home page" />
+      </Helmet>
 
       <MainHomeSection />
 
